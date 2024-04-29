924ef719c22300cb3cf6f11fcfb73cad12698fdc
seedee <cdaniel9000@gmail.com>
2024-04-29 22:24:31 +0100

    Add min value to equipment quantity

--- inventory_management/inventory_management/forms.py
+++ inventory_management/inventory_management/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Equipment
 
 class EquipmentFilterForm(forms.Form):
-    quantity = forms.IntegerField(required=True)
+    quantity = forms.IntegerField(min_value=1, required=True)
     audit = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
     equipment_type = forms.ChoiceField(choices=Equipment.EQUIPMENT_TYPES, required=False)
     location = forms.ChoiceField(choices=Equipment.LOCATIONS, required=False)
